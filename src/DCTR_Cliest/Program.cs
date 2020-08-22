62d533c29d4c18cde6e5848ef48a093c73ca1087
degid <sdegid@gmail.com>
2020-08-23 00:36:45 +0300

    created a service template

---
+++ src/DCTR_Cliest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Diagnostics;
+using System.Text;
+
+
+using System.ComponentModel;
+using System.Data;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.IsolatedStorage;
+
+namespace DCTR_Cliest
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        static void Main()
+        {
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Cliest()
+            };
+            ServiceBase.Run(ServicesToRun);
+        }
+    }
+}
